2185c02f0d691f56f2b47d1dfe095042e03646e5
Fazrul Rahman <fzrlrhmn@gmail.com>
2018-05-29 10:40:04 +0530

    Minor change

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -12,7 +12,7 @@ auth.set_access_token(access_token,access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.search('thoothukudi')
+public_tweets = api.search('apple')
 
 storage_dir = "repo.csv"
 csv = open(storage_dir,"w")
